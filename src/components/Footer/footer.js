e7919d34b347f10bae2b87c74b89ba69d34b14da
prakatesh <prakatesh2020@gmail.com>
2023-04-06 22:38:04 +0530

    card updated in home

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Row,Col} from 'react-bootstrap'
 import style from '../../assets/Styles/Home/con4.module.css'
+import { NavLink } from 'react-router-dom'
 export default function Footer() {
   return (
    <div className={style.f}>  
@@ -10,23 +11,23 @@ export default function Footer() {
     </Col>
     <Col md={3}>
     <h3>Quick Contact</h3>
-    <p> New Bharathavel Sago Factory</p>
-    <p> Mr. M. Ganesan</p>
-    <p> New Bharathavel Sago Factory Sitheri Po, Attur, Salem, Tamil Nadu - 636101, India</p>
-    <p>+91-9443327857</p>
-    <p>NEWBHARATHAVELSAGO@GMAIL.COM
+    <p>Sri Elumalaiyan Sago Factory</p>
+    <p> Mr R Balakrishnan</p>
+    <p> Sri Elumalaiyan Sago Factory, Attur, Salem, Tamil Nadu - 636101, India</p>
+    <p>+91-8610126966</p>
+    <p>srielumalaiyansago@gmail.com
     </p>
     </Col>
     <Col md={1}>
     </Col>
     <Col md={2}>
     <h3>General Links</h3>
-    <p>Home</p>
-    <p>Products</p>
-    <p>Contact Us</p>
-    <p>About Us</p>
-    <p>Testimonilas</p>
-    <p>Site Map</p>
+    <NavLink>Home</NavLink>
+    <NavLink>Products</NavLink>
+    <NavLink>Contact Us</NavLink>
+    <NavLink>About Us</NavLink>
+    <NavLink>Testimonilas</NavLink>
+    <NavLink>Site Map</NavLink>
     </Col>
 
     <Col md={1}>
@@ -34,8 +35,8 @@ export default function Footer() {
 
     <Col md={3}>
     <h3>Products</h3>
-    <p>Sago Seeds</p>
-    <p>Tapioca Starch</p>
+    <NavLink>Sago Seeds</NavLink>
+    <NavLink>Tapioca Starch</NavLink>
     </Col>
 
     <Col md={2}>
--- src/components/Home/Con4.js
+++ src/components/Home/Con4.js
@@ -8,12 +8,12 @@ export default function Con4() {
     <div>
     <br/>
     <Row className={style.con4} >
-    <Col md={3}>
+    <Col md={2}>
     </Col>
-    <Col style={{boxShadow:"rgba(0, 0, 0, 0.24) 0px 3px 8px"}} md={4}>
+    <Col style={{boxShadow:"rgba(0, 0, 0, 0.24) 0px 3px 8px",margin:"1rem"}} md={4}>
     <img className="d-block w-100" alt="suma" src={sag2} />
     </Col>
-    <Col style={{boxShadow:"rgba(0, 0, 0, 0.24) 0px 3px 8px"}} md={4}>
+    <Col style={{boxShadow:"rgba(0, 0, 0, 0.24) 0px 3px 8px",margin:"1rem"}} md={4}>
     <h3>Sago Seeds</h3>
     <p><b>Country of Origin: </b>India</p>
     <p><b>Type: </b>Sago Seeds</p>
@@ -25,16 +25,15 @@ export default function Con4() {
     </Col>
     </Row>
     <Row  className={style.con4}>
-    <Col md={3}>
+    <Col md={2}>
     </Col>
-    
-    <Col style={{boxShadow:"rgba(0, 0, 0, 0.24) 0px 3px 8px"}}  md={4}>
+    <Col style={{boxShadow:"rgba(0, 0, 0, 0.24) 0px 3px 8px",margin:"1rem"}}  md={4}>
     <img className="d-block w-100" alt="suma" src={newsag} />
     </Col>
-    <Col style={{boxShadow:"rgba(0, 0, 0, 0.24) 0px 3px 8px"}}  md={4}>
+    <Col style={{boxShadow:"rgba(0, 0, 0, 0.24) 0px 3px 8px",margin:"1rem"}}  md={4}>
     <h3>Tapioca Starch</h3>
     <p><b>Country of Origin: </b>India</p>
-    <p><b>Type: </b>Tapioca Starch</p>
+    <p><b>Type:</b>Tapioca Starch</p>
     <p><b>Application: </b>Food Industry</p>
     <p><b>Cultivation Type: </b>Natural</p>
     <p><b>Color: </b>White</p><br></br>
