6af020d2739938db7adbee7a5f453fec3810622a
prakatesh <prakatesh2020@gmail.com>
2023-04-22 10:49:25 +0530

    connected with cloud(mongoDB)

--- server/app.js
+++ server/app.js
@@ -5,11 +5,28 @@ const bodyParser = require('body-parser')
 require('./connect')
 const userRouter=require('./Router/userRouter')
 const adminRouter=require('./Router/adminRouter')
+const manageModel= require('./model/manage')
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// app.get('/sal',(req,res)=>{
+//     var sum=0
+//     manageModel.find({})
+
+//     .then((data)=>{
+//         for(i of data)
+//         {
+//             sum=sum+i.TodayPurchase
+//         }
+//         console.log(sum)
+//         res.json({sum:sum})
+//         // console.log(data) 
+//     })
+    
+
+// })
 
 app.use('/user',userRouter)
 app.use('/admin',adminRouter)
--- server/connect.js
+++ server/connect.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/Sample")
+mongoose.connect("mongodb+srv://prakatesh2020:Kongu2022@cluster0.2taqg2c.mongodb.net/SES")
 .then(()=>{
     console.log("Db connected successfully")
 })
