a97eda85664661ff399c52677efbf1ac82823928
prakatesh <prakatesh2020@gmail.com>
2023-05-11 23:11:24 +0530

    change in navbar

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -3,8 +3,8 @@ import icon from '../../assets/Images/Icon/icon.png'
 
 
 import React from 'react';
-import {Collapse, Navbar,  NavbarToggler,  NavbarBrand,  Nav,  NavItem,NavLink} from 'reactstrap';
-
+import {Collapse, Navbar,  NavbarToggler,  NavbarBrand,  Nav,  NavItem} from 'reactstrap';
+import {NavLink} from 'react-router-dom'
 export default class Example extends React.Component {
   constructor(props) {
     super(props);
@@ -23,27 +23,27 @@ export default class Example extends React.Component {
     return (
       <div>
         <Navbar style={{backgroundColor:"rgb(47, 88, 117)"}}  light expand="md">
-          <NavbarBrand href="/"><img style={{width:"50%"}} src={icon} alt="icon"/></NavbarBrand>
+          <NavbarBrand to="/"><img style={{width:"50%"}} src={icon} alt="icon"/></NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/" style={{paddingLeft:"5vh",color:"white"}}><span style={{color:"white",paddingRight:"1vh"}}><i className="fa-solid fa-home" /></span>Home</NavLink>
+                <NavLink to="/" style={{paddingLeft:"5vh",color:"white",textDecoration:"none"}}><span style={{color:"white",paddingRight:"1vh"}}><i className="fa-solid fa-home" /></span>Home</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/about" style={{paddingLeft:"5vh",color:"white"}}><span style={{color:"white",paddingRight:"1vh"}}><i className="fa-solid fa-user" /></span>About</NavLink>
+                <NavLink to="/about" style={{paddingLeft:"5vh",color:"white",textDecoration:"none"}}><span style={{color:"white",paddingRight:"1vh"}}><i className="fa-solid fa-user" /></span>About</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/flipbook" style={{paddingLeft:"5vh",color:"white"}}><span style={{color:"white",paddingRight:"1vh"}}><i className="fa-solid fa-book" /></span>Flipbook</NavLink>
+                <NavLink to="/flipbook" style={{paddingLeft:"5vh",color:"white",textDecoration:"none"}}><span style={{color:"white",paddingRight:"1vh"}}><i className="fa-solid fa-book" /></span>Flipbook</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/product" style={{paddingLeft:"5vh",color:"white"}}><span style={{color:"white",paddingRight:"1vh"}}><i className="fa-solid fa-box-open" /></span>Product</NavLink>
+                <NavLink to="/product" style={{paddingLeft:"5vh",color:"white",textDecoration:"none"}}><span style={{color:"white",paddingRight:"1vh"}}><i className="fa-solid fa-box-open" /></span>Product</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/contact" style={{paddingLeft:"5vh",color:"white"}}><span style={{color:"white",paddingRight:"1vh"}}><i className="fa-solid fa-phone-volume"/></span>Contact us</NavLink>
+                <NavLink to="/contact" style={{paddingLeft:"5vh",color:"white",textDecoration:"none"}}><span style={{color:"white",paddingRight:"1vh"}}><i className="fa-solid fa-phone-volume"/></span>Contact us</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/admin/login" style={{paddingLeft:"5vh",color:"white"}}><span style={{color:"white",paddingRight:"1vh"}}><i className="fa-solid fa-lock" /></span>Admin</NavLink>
+                <NavLink to="/admin/login" style={{paddingLeft:"5vh",color:"white",textDecoration:"none"}}><span style={{color:"white",paddingRight:"1vh"}}><i className="fa-solid fa-lock" /></span>Admin</NavLink>
               </NavItem>
             </Nav>
           </Collapse>
